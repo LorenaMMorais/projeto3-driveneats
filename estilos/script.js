d452442e2ee1827e0b94ac6a8b1326853cf60238
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2022-01-30 20:43:25 -0300

    segunda versão sem JS

---
+++ estilos/script.js
@@ -0,0 +1,18 @@
+function selecionarPrato() {
+    
+}
+
+
+function selecionar() {
+    const selecaoPrato;
+    const selecaoBebida;
+    const selecaoSobremesa;
+    /*if(){
+        const finalizar = document.querySelector(".selecionando");
+        finalizar.innerHTML = "Fechar pedido";
+    }
+*/
+}
+function somarItensSelecionados(){
+
+}
